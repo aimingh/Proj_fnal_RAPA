eed1f914c21a136d0b8381ff51481b4f359da620
Junhee Cho <junhee3085@gmail.com>
2021-02-08 06:54:25 +0000

    visualize roi_floor

--- server/webRTC/utils.py
+++ server/webRTC/utils.py
@@ -14,11 +14,12 @@ class VideoCamera(object):
         self.move = False
         self.n = 0.0
         self.direction = ""
-        self.pw = 0.8
+        self.pw = 1
         self.left_power = (0.15)
         self.right_power = (0.145)
         # deep learning model setting
         self.set_model()
+        self.roi = [(0, 120),(80, 60),(160, 120),]
         # camera setting
         self.cap = cv2.VideoCapture(1)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # width
@@ -55,38 +56,46 @@ class VideoCamera(object):
         self.floor_rander[self.floor_rander[:,:]!=128] = 0
         self.floor_rander[self.floor_rander[:,:]==128] = 1
         self.floor_rander = self.floor_rander.reshape((120,320))
-        self.floor_mask = np.concatenate((self.floor_rander[:,:160], self.floor_rander[:,160:]), axis=0)
+
+        self.floor_rander[:,:160] = mask_roi(self.floor_rander[:,:160], self.roi)
+        self.floor_rander[:,160:] = mask_roi(self.floor_rander[:,160:], self.roi)
 
     def get_score(self):
         self.n = np.sum(self.floor_mask)
-        self.n_left = np.sum(self.floor_mask[:,:80])
-        self.n_right = self.n - self.n_left
+        self.n_left = np.sum(self.floor_rander[:,:80])
+        self.n_right = np.sum(self.floor_rander[:,240:])
+        
 
     def update_jetbot(self):
         if self.move:
-            if self.n > 2200:
+            if self.n > 6000:
                 self.direction = "Straight"
                 self.robot.set_motors(self.pw*self.left_power, self.pw*self.right_power)
-            elif self.n < 500:
+            elif self.n < 2000:
                 self.direction = "stop"
                 self.robot.stop()
-            elif self.n_left > self.n_right:
-                self.direction = "left"
-                self.robot.set_motors(-0.75*self.left_power, 0.75*self.right_power)
-            elif self.n_left < self.n_right:
+            elif self.n_right >= self.n_left + 300:
                 self.direction = "right"
                 self.robot.set_motors(0.75*self.left_power, -0.75*self.right_power)
+            elif self.n_left > self.n_right + 300:
+                self.direction = "left"
+                self.robot.set_motors(-0.75*self.left_power, 0.75*self.right_power)
             else:
-                self.direction = "stop"
+                self.direction = "Unknown"
                 self.robot.stop()
         else:
             self.robot.stop()
 
     def get_frame(self):
         image = self.img_rander
-        text = f'floor: {self.n:0.2f}, direction: {self.direction}'
+        text = f'floor: {self.n:0.2f}, left_n: {self.n_left}, right_n{self.n_right}, direction: {self.direction}'
         image = cv2.putText(image, text, (5,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA) 
         ret, jpeg = cv2.imencode('.jpg', image)
+        # roi visulization
+        tmp = cv2.resize(self.floor_rander, dsize=(0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+        tmp = np.stack((255*tmp,)*3,axis = 2)
+        result = np.concatenate((image, tmp),axis=0)
+        ret, jpeg = cv2.imencode('.jpg', result)
         return jpeg.tobytes()
 
     def update(self):
@@ -99,7 +108,7 @@ class VideoCamera(object):
             end = time.time() - start
             if end < 0.1:
                 time.sleep(0.1-end)
-            print("time :", end, ", total: ", time.time() - start)  # check time 
+            print("time :", end, ",\ttotal: ", time.time() - start)  # check time 
             
 class segmentationBuffers:
     def __init__(self, net):
@@ -111,3 +120,9 @@ class segmentationBuffers:
     def Alloc(self, shape, format):
         self.overlay = jetson.utils.cudaAllocMapped(width=shape[1], height=shape[0], format=format)
         self.mask = jetson.utils.cudaAllocMapped(width=shape[1]/self.mask_downsample, height=shape[0]/self.mask_downsample, format=format) 
+
+def mask_roi(img_th, roi):
+    mask = np.zeros_like(img_th)
+    cv2.fillPoly(mask, np.array([roi], np.int32), 255)
+    masked_image = cv2.bitwise_and(img_th, mask)
+    return masked_image
